0b38aef87174b697b2e621c24105d79c64a26730
Tahia Khan <tahia.khan@gmail.com>
2015-08-22 16:24:08 -0400

    updating

--- static/frontjs.js
+++
@@ -1,8 +0,0 @@
-$(document).ready( function () {
-	setTimeout(function() {
-		document.getElementById("shawlispeech").innerHTML ="my ultimate goal in life is to have cool things turn up when people google my name!<br><br>(you can move me by pressing the left and right keys)";
-		setTimeout(function() {
-			document.getElementById("shawlispeech").innerHTML = "please click the links above to find out more about me.<br><br>(you can move me by pressing the left and right keys) ";
-		}, 8000);
-	}, 10000);
-});
